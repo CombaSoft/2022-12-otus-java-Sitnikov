b4626b19dbcdf85dbd71ef103a4bf65c8b09fa02
CombaSoft <combasoft@yandex.ru>
2023-01-24 23:07:54 +0400

    Используем интерфейс, а не одну из его реализаций.

--- hw02/src/main/java/homework/CustomerReverseOrder.java
+++ hw02/src/main/java/homework/CustomerReverseOrder.java
@@ -7,7 +7,7 @@ public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    ArrayDeque<Customer> customerList = new ArrayDeque<>();
+    Deque<Customer> customerList = new ArrayDeque<>();
 
     public void add(Customer customer) {
         customerList.offerFirst(customer);
